b7a9a09ddb8db799f1397e18c508a3ab8796f8e1
Julien Heng <developor45ju@users.noreply.github.com>
2026-03-24 16:56:08 +0100

    Structured + Style + Begin interaction calculator

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-/* Place your JavaScript in this file */
+const displayResult = document.getElementById('display-result');
+const themesCalculator = [...document.getElementsByClassName('themes-calculator')];
+const keysCalculator = [...document.getElementsByClassName('btn')];
+
+let keys = [];
+let operators = [];
+
+keysCalculator.forEach(key => {
+    if (key.getAttribute('data-numbers')) {
+        keys.push(key);
+    }
+    if (key.getAttribute('data-operation')) {
+        operators.push(key);  
+    } 
+})
+
+keys.forEach(number => {
+    number.addEventListener('click', (e) => {
+        const targetElement = e.target;
+        const catchNumber = targetElement.getAttribute('data-numbers');
+        if (displayResult.innerText === '0') displayResult.innerHTML = '';
+        displayResult.innerHTML += catchNumber;
+    })
+})
